4e3998b26e23f953a226ec389fb2453a31cdb0be
kavya <kavyavelisetti@gmail.com>
2019-07-25 21:08:03 +0530

    Counter App is up and running!

---
+++ src/components/navbar.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+class NavBar extends Component {
+  render() {
+    return (
+      <nav className="navbar navbar-light bg-light">
+        <span className="navbar-brand mb-0 h1">
+          Navbar
+          <span className="badge badge-pill badge-secondary m-3">
+            {this.props.totalCounters}
+          </span>
+        </span>
+      </nav>
+    );
+  }
+}
+
+export default NavBar;
